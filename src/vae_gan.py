709fe19a715facef94baf0c09df0032b0a663288
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-03 08:17:55 -0700

    Fixed logging for vae/gan training.

--- src/vae_gan.py
+++ src/vae_gan.py
@@ -30,29 +30,41 @@ class VAE_GAN(nn.Module):
         batch_count = len(self.dataloader)
 
         for epoch in range(epochs):
-            if epoch_callback is not None:
-                epoch_callback(epoch)
+            recons_loss_total = 0
+            kld_loss_total = 0
 
             for batch_number, sample in enumerate(self.dataloader):
                 sample = Variable(sample[0].type(FloatTensor))
                 recons_loss, kld_loss = self.vae.train_batch(sample)
 
+                recons_loss_total += recons_loss / batch_count
+                kld_loss_total += kld_loss / batch_count
+
                 print('[Epoch {}/{}] (Batch {}/{}) Recon_Loss: {:.4f}, KLD_Loss: {:.4f}'
                       .format(epoch + 1, epochs, batch_number + 1, batch_count, recons_loss, kld_loss))
+
+            if epoch_callback is not None:
+                epoch_callback(epoch, recons_loss_total, kld_loss_total)
 
     def train_gan(self, epochs=100, epoch_callback=None):
         batch_count = len(self.dataloader)
 
         for epoch in range(epochs):
-            if epoch_callback is not None:
-                epoch_callback(epoch)
+            g_loss_total = 0
+            d_loss_total = 0
 
             for batch_number, sample in enumerate(self.dataloader):
                 sample = Variable(sample[0].type(FloatTensor))
                 d_loss, g_loss = self.gan.train_batch(sample)
 
+                g_loss_total += g_loss / batch_count
+                d_loss_total += d_loss / batch_count
+
                 print('[Epoch {}/{}] (Batch {}/{}) G_Loss: {:.4f}, D_Loss: {:.4f}'
                       .format(epoch + 1, epochs, batch_number + 1, batch_count, g_loss, d_loss))
+
+            if epoch_callback is not None:
+                epoch_callback(epoch, g_loss_total, d_loss_total)
 
     def train_dual(self, epochs=100, epoch_callback=None):
         batch_count = len(self.dataloader)
