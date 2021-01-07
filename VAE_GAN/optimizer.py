525dbc940baaaa2aae638d4fd51c445c9d277965
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-07 05:39:43 -0700

    Removed learnable batch size.

--- VAE_GAN/optimizer.py
+++ VAE_GAN/optimizer.py
@@ -7,8 +7,8 @@ from .model import Model, ModelParameters
 def optimize(parameters: ModelParameters, trial_count):
     def trial(trial):
         p2 = copy(parameters)
-        p2.vae_pretraining_epochs = 1
-        p2.epochs = 3
+        p2.vae_pretraining_epochs = 2
+        p2.epochs = 5
         p2.save_model = False
         p2.save_snapshots = False
         p2.print_summary = False
@@ -18,7 +18,6 @@ def optimize(parameters: ModelParameters, trial_count):
         p2.gan_learning_rate = trial.suggest_loguniform('gan_lr', 1e-5, 1e-2)
         p2.vae_learning_rate = trial.suggest_loguniform('vae_lr', 1e-5, 1e-2)
         p2.layers_per_size = trial.suggest_int('layers', 1, 4)
-        p2.batch_size = trial.suggest_int('batch_size', 1, 22)
         p2.latent_dim = trial.suggest_int('latent', 32, 1600)
 
         def epoch_callback(epoch, recons_loss, kld_loss, g_loss, d_loss):
@@ -39,7 +38,6 @@ def optimize(parameters: ModelParameters, trial_count):
     parameters.gan_learning_rate = best['gan_lr']
     parameters.vae_learning_rate = best['vae_lr']
     parameters.layers_per_size = best['layers']
-    parameters.batch_size = best['batch_size']
     parameters.latent_dim = best['latent']
 
     return parameters
