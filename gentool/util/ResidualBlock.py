20d09b7e42c7ef38fed9ef7c5308b663a0bd6581
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-09 21:52:08 -0700

    Added configurable normalization.

--- gentool/util/ResidualBlock.py
+++ gentool/util/ResidualBlock.py
@@ -1,8 +1,49 @@
+from math import floor, log2
 from torch import nn
 
 
+def best_group_count(channels):
+    target_channels = min(16, int(channels / 3))
+    target_dist = floor(log2(target_channels) + 0.5)
+
+    best_count = 1
+    best_dist = target_dist
+
+    for i in range(1, channels + 1):
+        if channels % i != 0:
+            continue
+
+        dist = abs(floor(log2(i) + 0.5) - target_dist)
+        if dist < best_dist:
+            best_count = i
+            best_dist = dist
+
+    return best_count
+
+
+def get_normalization(norm_name, channels, image_size, learnable_params):
+    if norm_name == 'none':
+        return nn.Identity()
+
+    if norm_name == 'batch':
+        return nn.BatchNorm2d(channels)
+
+    if norm_name == 'group':
+        groups = channels / 16
+        return nn.GroupNorm(groups, channels)
+
+    if norm_name == 'layer':
+        return nn.LayerNorm((channels, image_size, image_size), elementwise_affine=learnable_params)
+
+    if norm_name == 'instance':
+        return nn.InstanceNorm2d(channels, affine=learnable_params)
+
+    raise ValueError("Normalization '{}' not found!".format(norm_name))
+
+
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, activation, bias=True, kernel=3):
+    def __init__(self, in_channels, out_channels, image_size, activation, bias=True, kernel=3,
+                 normalization='batch', learnable_params=True):
         super().__init__()
 
         self.in_channels = in_channels
@@ -12,15 +53,13 @@ class ResidualBlock(nn.Module):
 
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels, hidden_channels, kernel, 1, 1, bias=bias),
-            # nn.GroupNorm(3, hidden_channels),
-            nn.BatchNorm2d(hidden_channels),
+            get_normalization(normalization, hidden_channels, image_size, learnable_params),
             activation,
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(hidden_channels, out_channels, kernel, 1, 1, bias=bias),
-            nn.BatchNorm2d(out_channels),
-            # nn.GroupNorm(3, out_channels),
+            get_normalization(normalization, out_channels, image_size, learnable_params),
         )
 
         self.activation = activation
