a3374c0673c81d427c6f0e93b56a16d5a14df8ad
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-12 07:37:41 -0700

    image_dataset now loads from list of folders.

--- gentool/database/image_dataset.py
+++ gentool/database/image_dataset.py
@@ -1,27 +1,32 @@
-import os
+from os import listdir
+from os.path import join as join_path
 
-import torch
+from random import randint
 from torch.utils.data import Dataset
-
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 
 class ImageDataset(Dataset):
-    def __init__(self, dir, transform, format='RGB'):
-        self.dir = dir
+    def __init__(self, folders, transform, format='RGB'):
         self.transform = transform
         self.format = format
-        self.files = os.listdir(self.dir)
+        self.files = []
+
+        for folder in folders:
+            for file in listdir(folder):
+                self.files.append(join_path(folder, file))
 
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, index):
-        if torch.is_tensor(index):
-            index = index.tolist()
+        img_name = self.files[index]
 
-        img_name = os.path.join(self.dir, self.files[index])
-        image = Image.open(img_name).convert(self.format)
-        image = self.transform(image)
-
-        return [image]
+        try:
+            image = Image.open(img_name).convert(self.format)
+            image = self.transform(image)
+            return image
+        except UnidentifiedImageError:
+            # File cannot be loaded. Remove from file list
+            self.files.remove(img_name)
+            return self.__getitem__(randint(0, len(self.files) - 1))
