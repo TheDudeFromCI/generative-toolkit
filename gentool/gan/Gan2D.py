5ea056e76c2dc75a360735fdcffd2f0eec0dcf6a
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-13 12:07:11 -0700

    Reorganized class.

--- gentool/gan/Gan2D.py
+++ gentool/gan/Gan2D.py
@@ -1,17 +1,16 @@
-from gentool.ModelBase import ImageModelBase
-
 import numpy as np
 from math import log2
 
 import torch
 from torch import nn
+from torchinfo import summary
+from torch.optim.adam import Adam
 from torch.cuda import FloatTensor
 from torch.autograd import Variable
-from torch.optim.adam import Adam
-import torch.nn.functional as F
-import torchvision.transforms as T
-from torchinfo import summary
+from torch.nn import functional as F
+from torchvision import transforms as T
 
+from gentool.ModelBase import ImageModelBase
 from gentool.util.ImageToVec import ImageToVec
 from gentool.util.VecToImage import VecToImage
 from gentool.database.ImageDataLoader import image_dataloader
@@ -23,7 +22,6 @@ class Gan2DHyperParameters():
         self.image_channels = 3
         self.generator_layers_per_size = 3
         self.discriminator_layers_per_size = 3
-        self.latent_dim = 32
         self.initial_channels = 4
         self.learning_rate = 1e-4
         self.kernel = 5
@@ -34,44 +32,54 @@ class Gan2DHyperParameters():
         self.dropout = 0.4
         self.normalization = 'group'
 
+    @property
+    def latent_dim(self):
+        return (self.initial_channels << int(log2(self.image_size) - 2)) * 4 * 4
+
 
 class Gan2D(ImageModelBase):
     def __init__(self, hyper_parameters: Gan2DHyperParameters):
         dataloader = image_dataloader(hyper_parameters.image_folders, hyper_parameters.batch_size,
                                       hyper_parameters.data_augmentation, workers=hyper_parameters.data_workers)
         super().__init__(dataloader)
-
         self.hyper_parameters = hyper_parameters
-        hyper_parameters.latent_dim = (hyper_parameters.initial_channels << int(
-            log2(hyper_parameters.image_size) - 1)) * 16
-
-        self.generator = VecToImage(hyper_parameters.image_size,
-                                    hyper_parameters.image_channels,
-                                    hyper_parameters.generator_layers_per_size,
-                                    initial_channels=hyper_parameters.initial_channels,
-                                    kernel=hyper_parameters.kernel,
-                                    dropout=hyper_parameters.dropout,
-                                    normalization=hyper_parameters.normalization,
-                                    min_size=4)
-
-        self.discriminator = ImageToVec(hyper_parameters.image_size,
-                                        hyper_parameters.image_channels,
-                                        hyper_parameters.discriminator_layers_per_size,
-                                        initial_channels=hyper_parameters.initial_channels,
-                                        kernel=hyper_parameters.kernel,
-                                        dropout=hyper_parameters.dropout,
-                                        normalization=hyper_parameters.normalization,
-                                        min_size=1)
-
-        self.discriminator.conv.insert(len(self.discriminator.conv) - 2, nn.LeakyReLU(inplace=True))
-        self.discriminator.conv.insert(len(self.discriminator.conv) - 2, nn.Conv2d(
-            hyper_parameters.initial_channels << int(log2(hyper_parameters.image_size)), 1, 1, 1))
+        self.generator = self._build_generator()
+        self.discriminator = self._build_discriminator()
+        self.dense = self._build_dense()
 
         self.optimizer_g = Adam(self.generator.parameters(), lr=hyper_parameters.learning_rate)
         self.optimizer_d = Adam(self.discriminator.parameters(), lr=hyper_parameters.learning_rate)
 
         self.cuda()
         summary(self, (1, hyper_parameters.latent_dim), depth=3)
+
+    def _build_generator(self):
+        return VecToImage(self.hyper_parameters.image_size,
+                          self.hyper_parameters.image_channels,
+                          self.hyper_parameters.generator_layers_per_size,
+                          initial_channels=self.hyper_parameters.initial_channels,
+                          kernel=self.hyper_parameters.kernel,
+                          dropout=self.hyper_parameters.dropout,
+                          normalization=self.hyper_parameters.normalization,
+                          min_size=4)
+
+    def _build_discriminator(self):
+        return ImageToVec(self.hyper_parameters.image_size,
+                          self.hyper_parameters.image_channels,
+                          self.hyper_parameters.discriminator_layers_per_size,
+                          initial_channels=self.hyper_parameters.initial_channels,
+                          kernel=self.hyper_parameters.kernel,
+                          dropout=self.hyper_parameters.dropout,
+                          normalization=self.hyper_parameters.normalization,
+                          min_size=4,
+                          output_activation=nn.LeakyReLU(inplace=True),
+                          normalize_last=True)
+
+    def _build_dense(self):
+        return nn.Sequential(
+            nn.Linear(self.hyper_parameters.latent_dim, 1),
+            nn.Tanh()
+        )
 
     def sample_images(self, count):
         z = Variable(FloatTensor(np.random.normal(0, 1, (count, self.hyper_parameters.latent_dim))))
@@ -96,34 +104,28 @@ class Gan2D(ImageModelBase):
 
     def train_generator(self, batch):
         self.optimizer_g.zero_grad()
+        self.optimizer_d.zero_grad()
 
         fake_input_noise = torch.randn_like(batch)
         g_fake_data = self.generator(self.z_noise()) + fake_input_noise * 0.2
         dg_fake_decision = self.discriminator(g_fake_data)
+        dg_fake_decision = self.dense(dg_fake_decision)
 
         g_loss = F.mse_loss(dg_fake_decision, self.real_label())
         g_loss.backward()
         self.optimizer_g.step()
 
-        return g_loss.item()
-
-    def train_discriminator_fake(self, batch):
-        self.optimizer_d.zero_grad()
-
-        fake_input_noise = torch.randn_like(batch)
-        g_fake_data = self.generator(self.z_noise()) + fake_input_noise * 0.2
-        d_real_decision = self.discriminator(g_fake_data)
-
-        d_loss = F.mse_loss(d_real_decision, self.fake_label())
+        d_loss = F.mse_loss(dg_fake_decision, self.fake_label())
         d_loss.backward()
         self.optimizer_d.step()
 
-        return d_loss.item()
+        return g_loss.item(), d_loss.item()
 
     def train_discriminator_real(self, batch):
         self.optimizer_d.zero_grad()
 
         d_real_decision = self.discriminator(batch + torch.randn_like(batch) * 0.2)
+        d_real_decision = self.dense(d_real_decision)
 
         d_loss = F.mse_loss(d_real_decision, self.real_label())
         d_loss.backward()
@@ -132,12 +134,10 @@ class Gan2D(ImageModelBase):
         return d_loss.item()
 
     def train_batch(self, batch):
-        g_loss = self.train_generator(batch)
-
-        d_loss_fake = self.train_discriminator_fake(batch)
+        g_loss, d_loss_fake = self.train_generator(batch)
         d_loss_real = self.train_discriminator_real(batch)
-        d_loss = (d_loss_real + d_loss_fake) / 2
 
+        d_loss = (d_loss_real + d_loss_fake) / 2
         return [g_loss, d_loss]
 
     def loss_names_and_groups(_):
@@ -146,4 +146,5 @@ class Gan2D(ImageModelBase):
     def forward(self, x):
         x = self.generator(x)
         x = self.discriminator(x)
+        x = self.dense(x)
         return x
