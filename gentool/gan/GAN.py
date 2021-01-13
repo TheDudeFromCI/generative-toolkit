51cdeaabab4a6242525008ef50acef8998afd140
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-13 07:32:45 -0700

    Refactyored train_batch.

--- gentool/gan/GAN.py
+++ gentool/gan/GAN.py
@@ -73,45 +73,68 @@ class GAN(ImageModelBase):
         z = Variable(FloatTensor(np.random.normal(0, 1, (count, self.hyper_parameters.latent_dim))))
         return self.generator(z)
 
-    def train_batch(self, batch):
-        # Initialize
-        batch_size = len(batch)
-        ones = Variable(FloatTensor(np.ones((batch_size, 1)))) * 0.8
-        z_noise = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, self.hyper_parameters.latent_dim))))
-        real_input_noise = torch.randn_like(batch)
-        fake_input_noise = torch.randn_like(batch)
+    def z_noise(self):
+        batch_size = self.hyper_parameters.batch_size
+        latent_dim = self.hyper_parameters.latent_dim
+        return Variable(FloatTensor(np.random.normal(0, 1, (batch_size, latent_dim))))
 
-        # Update generator
+    def real_label(self):
+        batch_size = self.hyper_parameters.batch_size
+        ones = Variable(FloatTensor(np.ones((batch_size, 1))))
+        label_noise = Variable(FloatTensor(np.random.normal(0, 0.2, (batch_size, 1))))
+        return ones * 0.8 + label_noise
+
+    def fake_label(self):
+        batch_size = self.hyper_parameters.batch_size
+        ones = Variable(FloatTensor(np.ones((batch_size, 1))))
+        label_noise = Variable(FloatTensor(np.random.normal(0, 0.2, (batch_size, 1))))
+        return ones * -0.8 + label_noise
+
+    def train_generator(self, batch):
         self.optimizer_g.zero_grad()
-        g_fake_data = self.generator(z_noise) + fake_input_noise
+
+        fake_input_noise = torch.randn_like(batch)
+        g_fake_data = self.generator(self.z_noise()) + fake_input_noise * 0.2
         dg_fake_decision = self.discriminator(g_fake_data)
 
-        label_noise = Variable(FloatTensor(np.random.normal(0, 0.2, (batch_size, 1))))
-        g_loss = F.mse_loss(dg_fake_decision, -ones + label_noise)
+        g_loss = F.mse_loss(dg_fake_decision, self.real_label())
         g_loss.backward()
         self.optimizer_g.step()
 
-        # Update discriminator (Fake Data)
-        g_fake_data = Variable(g_fake_data.data)
+        return g_loss.item()
 
+    def train_discriminator_fake(self, batch):
         self.optimizer_d.zero_grad()
+
+        fake_input_noise = torch.randn_like(batch)
+        g_fake_data = self.generator(self.z_noise()) + fake_input_noise * 0.2
         d_real_decision = self.discriminator(g_fake_data)
 
-        label_noise = Variable(FloatTensor(np.random.normal(0, 0.2, (batch_size, 1))))
-        d_loss = F.mse_loss(d_real_decision, -ones + label_noise)
+        d_loss = F.mse_loss(d_real_decision, self.fake_label())
         d_loss.backward()
         self.optimizer_d.step()
 
-        # Update discriminator (Real Data)
+        return d_loss.item()
+
+    def train_discriminator_real(self, batch):
         self.optimizer_d.zero_grad()
-        d_real_decision = self.discriminator(batch + real_input_noise)
 
-        label_noise = Variable(FloatTensor(np.random.normal(0, 0.2, (batch_size, 1))))
-        d_loss = F.mse_loss(d_real_decision, ones + label_noise)
+        d_real_decision = self.discriminator(batch + torch.randn_like(batch) * 0.2)
+
+        d_loss = F.mse_loss(d_real_decision, self.real_label())
         d_loss.backward()
         self.optimizer_d.step()
 
-        return [g_loss.item(), d_loss.item()]
+        return d_loss.item()
 
-    def loss_names_and_groups(self):
+    def train_batch(self, batch):
+        g_loss = self.train_generator(batch)
+
+        d_loss_fake = self.train_discriminator_fake(batch)
+        d_loss_real = self.train_discriminator_real(batch)
+        d_loss = (d_loss_real + d_loss_fake) / 2
+
+        return [g_loss, d_loss]
+
+    def loss_names_and_groups(_):
         return ['g_loss', 'd_loss'], {'GAN': ['g_loss', 'd_loss']}
