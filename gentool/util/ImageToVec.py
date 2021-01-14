3c3dfea21da4feb95489c21775d8fd35dbc452ec
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-14 06:13:12 -0700

    Made bias optional

--- gentool/util/ImageToVec.py
+++ gentool/util/ImageToVec.py
@@ -5,7 +5,8 @@ from torch import nn
 class ImageToVec(nn.Module):
     def __init__(self, image_size, image_channels, layers_per_size, initial_channels=4,
                  activation=nn.LeakyReLU(inplace=True), output_activation=nn.Tanh(),
-                 dropout=0.4, kernel=3, normalization='group', min_size=4, normalize_last=False):
+                 dropout=0.4, kernel=3, normalization='group', min_size=4, normalize_last=False,
+                 bias=False):
         super().__init__()
 
         blocks = []
@@ -24,20 +25,22 @@ class ImageToVec(nn.Module):
 
                 if i == layers_per_size - 1:
                     c_old, channels = channels, int(channels * 2)
-                    blocks.append(nn.Conv2d(c_old, channels, 3, 2, 1))
+                    blocks.append(nn.Conv2d(c_old, channels, 3, 2, 1, bias=bias))
+                    blocks.append(get_normalization(normalization, channels, image_size, True))
+                    blocks.append(nn.Dropout2d(dropout))
+                    blocks.append(activation)
 
-                    if image_size > min_size:
-                        blocks.append(get_normalization(normalization, channels, image_size, True))
-                        blocks.append(activation)
-                        blocks.append(nn.Dropout2d(dropout))
-                    else:
-                        if normalize_last:
-                            blocks.append(get_normalization(normalization, channels, image_size, True))
-                        blocks.append(output_activation)
-
-        for i in range(layers_per_size):
-            blocks.append(ResidualBlock(channels, channels, image_size, kernel=kernel,
+        for i in range(layers_per_size - 1):
+            blocks.append(ResidualBlock(channels, channels, image_size, kernel=kernel, bias=bias,
                                         activation=activation, normalization=normalization, dropout=dropout))
+
+        blocks.append(nn.Conv2d(channels, channels, kernel, 1, int(kernel/2), bias=bias))
+
+        if normalize_last:
+            blocks.append(get_normalization(normalization, channels, image_size, True))
+            blocks.append(nn.Dropout2d(dropout))
+
+        blocks.append(output_activation)
 
         blocks.append(nn.Flatten())
         self.conv = nn.ModuleList(blocks)
--- gentool/util/VecToImage.py
+++ gentool/util/VecToImage.py
@@ -7,7 +7,8 @@ from torch import nn
 class VecToImage(nn.Module):
     def __init__(self, image_size, image_channels, layers_per_size, initial_channels=4,
                  activation=nn.LeakyReLU(inplace=True), output_activation=nn.Tanh(),
-                 dropout=0.4, kernel=3, normalization='group', min_size=4, normalize_last=False):
+                 dropout=0.4, kernel=3, normalization='group', min_size=4, normalize_last=False,
+                 bias=False):
         super().__init__()
 
         blocks = []
@@ -22,24 +23,25 @@ class VecToImage(nn.Module):
             for i in range(layers_per_size):
                 blocks.append(ResidualBlock(channels, channels, min_size, skip_connections=False,
                                             activation=activation, normalization=normalization, dropout=dropout,
-                                            kernel=kernel))
+                                            kernel=kernel, bias=bias))
 
                 if i == layers_per_size - 1:
                     c_old, channels = channels, int(channels / 2)
-                    blocks.append(nn.ConvTranspose2d(c_old, channels, 4, 2, 1))
+                    blocks.append(nn.ConvTranspose2d(c_old, channels, 4, 2, 1, bias=bias))
                     blocks.append(get_normalization(normalization, channels, min_size, True))
-                    blocks.append(activation)
                     blocks.append(nn.Dropout2d(dropout))
+                    blocks.append(activation)
 
         for i in range(layers_per_size):
             blocks.append(ResidualBlock(channels, channels, min_size, skip_connections=False,
                                         activation=activation, normalization=normalization, dropout=dropout,
-                                        kernel=kernel))
+                                        kernel=kernel, bias=bias))
 
-        blocks.append(nn.Conv2d(channels, image_channels, kernel, 1, int(kernel/2)))
+        blocks.append(nn.Conv2d(channels, image_channels, kernel, 1, int(kernel/2), bias=bias))
 
         if normalize_last:
             blocks.append(get_normalization(normalization, image_channels, image_size, True))
+            blocks.append(nn.Dropout2d(dropout))
 
         blocks.append(output_activation)
 
