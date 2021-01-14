f9a5b9e051c46d6618ee7be89854fdf90301eb36
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-14 06:12:45 -0700

    Added more default props to gan

--- gentool/gan/Gan2D.py
+++ gentool/gan/Gan2D.py
@@ -31,6 +31,10 @@ class Gan2DHyperParameters():
         self.data_workers = 4
         self.dropout = 0.4
         self.normalization = 'group'
+        self.adam_beta1 = 0.5
+        self.adam_beta2 = 0.999
+        self.bias_neurons = False
+        self.leaky_relu_slope = 0.2
 
     @property
     def latent_dim(self):
@@ -47,8 +51,9 @@ class Gan2D(ImageModelBase):
         self.discriminator = self._build_discriminator()
         self.dense = self._build_dense()
 
-        self.optimizer_g = Adam(self.generator.parameters(), lr=hyper_parameters.learning_rate)
-        self.optimizer_d = Adam(self.discriminator.parameters(), lr=hyper_parameters.learning_rate)
+        betas = (hyper_parameters.adam_beta1, hyper_parameters.adam_beta2)
+        self.optimizer_g = Adam(self.generator.parameters(), lr=hyper_parameters.learning_rate, betas=betas)
+        self.optimizer_d = Adam(self.discriminator.parameters(), lr=hyper_parameters.learning_rate, betas=betas)
 
         self.cuda()
         summary(self, (1, hyper_parameters.latent_dim), depth=3)
@@ -61,7 +66,11 @@ class Gan2D(ImageModelBase):
                           kernel=self.hyper_parameters.kernel,
                           dropout=self.hyper_parameters.dropout,
                           normalization=self.hyper_parameters.normalization,
-                          min_size=4)
+                          min_size=4,
+                          activation=nn.ReLU(inplace=True),
+                          output_activation=nn.Tanh(),
+                          normalize_last=False,
+                          bias=self.hyper_parameters.bias_neurons)
 
     def _build_discriminator(self):
         return ImageToVec(self.hyper_parameters.image_size,
@@ -72,18 +81,23 @@ class Gan2D(ImageModelBase):
                           dropout=self.hyper_parameters.dropout,
                           normalization=self.hyper_parameters.normalization,
                           min_size=4,
-                          output_activation=nn.LeakyReLU(inplace=True),
-                          normalize_last=True)
+                          activation=nn.LeakyReLU(self.hyper_parameters.leaky_relu_slope, inplace=True),
+                          output_activation=nn.LeakyReLU(self.hyper_parameters.leaky_relu_slope, inplace=True),
+                          normalize_last=True,
+                          bias=self.hyper_parameters.bias_neurons)
 
     def _build_dense(self):
         return nn.Sequential(
-            nn.Linear(self.hyper_parameters.latent_dim, 1),
-            nn.Tanh()
+            nn.Linear(self.hyper_parameters.latent_dim, 1, bias=self.hyper_parameters.bias_neurons),
+            nn.LeakyReLU(self.hyper_parameters.leaky_relu_slope, inplace=True)
         )
 
     def sample_images(self, count):
         z = Variable(FloatTensor(np.random.normal(0, 1, (count, self.hyper_parameters.latent_dim))))
-        return self.generator(z)
+        generated = self.generator(z)
+        batch = next(self.dataloader)
+
+        return torch.cat([generated, batch])
 
     def z_noise(self):
         batch_size = self.hyper_parameters.batch_size
@@ -93,19 +107,20 @@ class Gan2D(ImageModelBase):
     def real_label(self):
         batch_size = self.hyper_parameters.batch_size
         ones = Variable(FloatTensor(np.ones((batch_size, 1))))
-        label_noise = Variable(FloatTensor(np.random.normal(0, 0.2, (batch_size, 1))))
-        return ones * 0.8 + label_noise
+        label_noise = Variable(FloatTensor(np.random.normal(0, 0.05, (batch_size, 1))))
+        return ones * 0.9 + label_noise
 
     def fake_label(self):
         batch_size = self.hyper_parameters.batch_size
         ones = Variable(FloatTensor(np.ones((batch_size, 1))))
-        label_noise = Variable(FloatTensor(np.random.normal(0, 0.2, (batch_size, 1))))
-        return ones * -0.8 + label_noise
+        label_noise = Variable(FloatTensor(np.random.normal(0, 0.05, (batch_size, 1))))
+        return ones * 0.1 + label_noise
 
     def train_generator(self, batch):
         self.optimizer_g.zero_grad()
-        fake_input_noise = torch.randn_like(batch)
-        g_fake_data = self.generator(self.z_noise()) + fake_input_noise * 0.2
+
+        fake_input_noise = torch.randn_like(batch) * 0.1
+        g_fake_data = self.generator(self.z_noise()) + fake_input_noise
         dg_fake_decision = self.discriminator(g_fake_data)
         dg_fake_decision = self.dense(dg_fake_decision)
 
@@ -113,9 +128,13 @@ class Gan2D(ImageModelBase):
         g_loss.backward()
         self.optimizer_g.step()
 
+        return g_loss.item()
+
+    def train_discriminator_fake(self, batch):
         self.optimizer_d.zero_grad()
-        fake_input_noise = torch.randn_like(batch)
-        g_fake_data = self.generator(self.z_noise()) + fake_input_noise * 0.2
+
+        fake_input_noise = torch.randn_like(batch) * 0.1
+        g_fake_data = self.generator(self.z_noise()) + fake_input_noise
         dg_fake_decision = self.discriminator(g_fake_data)
         dg_fake_decision = self.dense(dg_fake_decision)
 
@@ -123,12 +142,13 @@ class Gan2D(ImageModelBase):
         d_loss.backward()
         self.optimizer_d.step()
 
-        return g_loss.item(), d_loss.item()
+        return d_loss.item()
 
     def train_discriminator_real(self, batch):
         self.optimizer_d.zero_grad()
 
-        d_real_decision = self.discriminator(batch + torch.randn_like(batch) * 0.2)
+        real_input_noise = torch.randn_like(batch) * 0.1
+        d_real_decision = self.discriminator(batch + real_input_noise)
         d_real_decision = self.dense(d_real_decision)
 
         d_loss = F.mse_loss(d_real_decision, self.real_label())
@@ -138,10 +158,12 @@ class Gan2D(ImageModelBase):
         return d_loss.item()
 
     def train_batch(self, batch):
-        g_loss, d_loss_fake = self.train_generator(batch)
         d_loss_real = self.train_discriminator_real(batch)
+        d_loss_fake = self.train_discriminator_fake(batch)
 
+        g_loss = self.train_generator(batch)
         d_loss = (d_loss_real + d_loss_fake) / 2
+
         return [g_loss, d_loss]
 
     def loss_names_and_groups(_):
