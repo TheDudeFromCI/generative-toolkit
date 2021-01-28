11dc6cef6f4841f094cb6d19230faf87b6b05345
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-27 23:15:08 -0700

    Added dataset generator tool.

--- gentool/tools/PreGenDataset.py
+++ gentool/tools/PreGenDataset.py
@@ -1,6 +1,8 @@
 from os import listdir, makedirs, remove
 from os.path import join, isfile
 from random import randint
+from multiprocessing import Process
+from math import ceil
 
 import numpy as np
 from tqdm import tqdm
@@ -19,26 +21,50 @@ def delete_file(file):
         for f in listdir(file):
             delete_file(join(file, f))
 
-def pre_generate_dataset(image_folders, transform, output_folder, sample_count, batch_size):
-    dataloader = image_dataloader(image_folders, batch_size,
-                                  transform, workers=1)
+
+def pre_generate_dataset_parallel(image_folders, transform, output_folder, sample_count, workers):
+    samples_each = ceil(sample_count / workers)
+    processes = []
+    for i in range(workers):
+        p = Process(target=pre_generate_dataset, args=(image_folders, transform, output_folder, samples_each, i * samples_each))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+def pre_generate_dataset(image_folders, transform, output_folder, sample_count, offset=0):
+    dataloader = image_dataloader(image_folders, 1, transform, workers=1)
 
     delete_file(output_folder)
     makedirs(output_folder, exist_ok=True)
     for i in tqdm(range(sample_count)):
-        batch = next(dataloader).cpu()
-
-        file = join(output_folder, str(i) + '.npy')
-        np.save(file, batch.numpy())
+        batch = next(dataloader)[0].cpu()
+        save_batch(batch, output_folder, i + offset)
 
 
-def load_dataset(folder):
+def save_batch(batch, output_folder, index):
+    file = join(output_folder, str(index) + '.npy')
+    np.save(file, batch.numpy())
+
+class MakeIter(object):
+    def __init__(self, generator_func, **kwargs):
+        self.generator_func = generator_func
+        self.kwargs = kwargs
+
+    def __iter__(self):
+        return self.generator_func(**self.kwargs)
+
+
+def load_dataset(folder, batch_size):
     files = [join(folder, file) for file in listdir(folder)]
 
-    def loader():
-        while True:
+    while True:
+        batch = []
+        for _ in range(batch_size):
             img_name = files[randint(0, len(files) - 1)]
-            batch = torch.from_numpy(np.load(img_name))
-            yield Variable(batch.type(FloatTensor))
-
-    return loader
+            image = torch.from_numpy(np.load(img_name))
+            batch.append(image)
+        
+        batch = torch.stack(batch)
+        yield Variable(batch.type(FloatTensor))
