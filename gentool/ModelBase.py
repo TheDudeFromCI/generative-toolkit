f66ed2600ace98ad642a54189b65c9a5d7bda565
TheDudeFromCI <thedudefromci@gmail.com>
2021-02-10 02:30:49 -0700

    Simplified unnessicarily complex model loading.

--- gentool/ModelBase.py
+++ gentool/ModelBase.py
@@ -16,131 +16,6 @@ from torchvision.utils import save_image
 from torch.autograd.variable import Variable
 
 
-def get_normalization_1d(name, channels):
-    flags = {}
-    if name.find(';') >= 0:
-        parts = name.split(';')
-        name = parts[0].strip()
-        flags = dict(item.split('=') for item in parts[1].strip().split(' '))
-
-    if name == 'batch':
-        return nn.BatchNorm1d(channels)
-
-    if name == 'group':
-        groups = int(flags['groups']) if 'groups' in flags else 16
-        return nn.GroupNorm(groups, channels)
-
-    if name == 'layer':
-        return nn.LayerNorm((channels,))
-
-    if name == 'instance':
-        return nn.InstanceNorm1d(channels)
-
-    if name == 'none':
-        return nn.Identity()
-
-    assert False
-
-
-def get_normalization(name, channels, image_size):
-    flags = {}
-    if name.find(';') >= 0:
-        parts = name.split(';')
-        name = parts[0].strip()
-        flags = dict(item.split('=') for item in parts[1].strip().split(' '))
-
-    if name == 'batch':
-        return nn.BatchNorm2d(channels)
-
-    if name == 'group':
-        groups = int(flags['groups']) if 'groups' in flags else 16
-        return nn.GroupNorm(groups, channels)
-
-    if name == 'layer':
-        return nn.LayerNorm((channels, image_size, image_size))
-
-    if name == 'instance':
-        return nn.InstanceNorm2d(channels)
-
-    if name == 'none':
-        return nn.Identity()
-
-    assert False
-
-
-def get_activation(name):
-    flags = {}
-    if name.find(';') >= 0:
-        parts = name.split(';')
-        name = parts[0].strip()
-        flags = dict(item.split('=') for item in parts[1].strip().split(' '))
-
-    if name == 'relu':
-        return nn.ReLU()
-
-    if name == 'leaky_relu':
-        slope = float(flags['slope']) if 'slope' in flags else 0.01
-        return nn.LeakyReLU(negative_slope=slope)
-
-    if name == 'sigmoid':
-        return nn.Sigmoid()
-
-    if name == 'tanh':
-        return nn.Tanh()
-
-    if name == 'softmax':
-        return nn.Softmax()
-
-    if name == 'none':
-        return nn.Identity()
-
-    assert False
-
-
-def conv2d(in_channels, out_channels, image_size, kernel_size=3, normalization='group', activation='leaky_relu', downsample=False,
-           transpose=False):
-    assert not (downsample and transpose), 'Cannot downsample and transpose at the same time!'
-
-    if transpose:
-        conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, 2, kernel_size // 2, 1)
-    else:
-        stride = 2 if downsample else 1
-        conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, kernel_size // 2)
-
-    if normalization == 'spectral':
-        conv = nn.utils.spectral_norm(conv)
-        norm = nn.Identity()
-    else:
-        norm = get_normalization(normalization, out_channels, image_size)
-
-    activ = get_activation(activation)
-
-    return nn.Sequential(conv, norm, activ)
-
-
-class SkipConnection(nn.Module):
-    def __init__(self, channels, image_size, kernel_size=3, normalization='group', activation='leaky_relu', skips=2):
-        super().__init__()
-
-        assert normalization != 'spectral', 'Cannot use spectral normalization with skip connections!'
-
-        self.conv1 = nn.Sequential(
-            *[conv2d(channels, channels, image_size, kernel_size=kernel_size, normalization=normalization)
-              for _ in range(skips - 1)],
-
-            nn.Conv2d(channels, channels, kernel_size, 1, kernel_size // 2),
-        )
-
-        self.conv2 = nn.Sequential(
-            get_normalization(normalization, channels, image_size),
-            get_activation(activation),
-        )
-
-    def forward(self, x):
-        x = self.conv1(x) + x
-        return self.conv2(x)
-
-
 class ModelBase(nn.Module):
     def __init__(self):
         super().__init__()
@@ -233,6 +108,10 @@ class ImageModelBase(ModelBase):
         generated = self(modified)
         images = [val for pair in zip(batch, modified, generated) for val in pair]
         return images, int(sqrt(len(batch))) * 3
+
+    def fit(self, n_updates, offset=0):
+        self.save_snapshot(update_number=offset)
+        super().fit(n_updates, offset)
 
 
 class VaeModelBase(ImageModelBase):
--- gentool/Models/StandardGan.py
+++ gentool/Models/StandardGan.py
@@ -1,24 +1,113 @@
+from math import log2
+
+from torch import nn
+
 from ModelBase import GanModelBase
 from Database import numpy_dataloader
-from NetworkLoader import load_network
+
+
+class ResidualBlockDown(nn.Module):
+    def __init__(self, in_channels, out_channels, image_size):
+        super().__init__()
+
+        self.shortcut = nn.Sequential(
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(in_channels, out_channels, 1, 1, 0),
+        )
+
+        self.conv = nn.Sequential(
+            nn.LayerNorm((in_channels, image_size, image_size)),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(in_channels, in_channels, 3, 1, 1, bias=False),
+            nn.LayerNorm((in_channels, image_size, image_size)),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            nn.AvgPool2d(2, 2),
+        )
+
+    def forward(self, x):
+        return self.conv(x) + self.shortcut(x)
+
+
+class ResidualBlockUp(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.shortcut = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, 4, 2, 1),
+        )
+
+        self.conv = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(in_channels, out_channels, 4, 2, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+        )
+
+    def forward(self, x):
+        return self.conv(x) + self.shortcut(x)
+
+
+def build_generator(image_channels, image_size, model_channels, max_channels, latent_dim):
+    upsamples = int(log2(image_size)) - 2
+
+    return nn.Sequential(
+        nn.Linear(latent_dim, min((1 << upsamples) * model_channels, max_channels) * 4 * 4),
+        nn.Unflatten(1, (min((1 << upsamples) * model_channels, max_channels), 4, 4)),
+
+        *[ResidualBlockUp(min((1 << i) * 2 * model_channels, max_channels),
+                          min((1 << i) * 1 * model_channels, max_channels))
+            for i in range(upsamples - 1, -1, -1)],
+
+        nn.BatchNorm2d(model_channels),
+        nn.LeakyReLU(),
+
+        nn.Conv2d(model_channels, image_channels, 3, 1, 1),
+        nn.Tanh(),
+    )
+
+
+def build_discriminator(image_channels, image_size, model_channels, max_channels):
+    downsamples = int(log2(image_size)) - 2
+
+    return nn.Sequential(
+        nn.Conv2d(image_channels, model_channels, 3, 1, 1),
+
+        *[ResidualBlockDown(min((1 << i) * 1 * model_channels, max_channels),
+                            min((1 << i) * 2 * model_channels, max_channels),
+                            image_size >> i)
+            for i in range(downsamples)],
+
+        nn.Flatten(),
+        nn.Linear(min((1 << downsamples) * model_channels, max_channels) * 4 * 4, 1),
+    )
 
 
 def build_standard_wgan_gp(config):
-    generator = load_network(config['generator'])
-    discriminator = load_network(config['discriminator'])
+    image_channels = config['image_channels']
+    image_size = config['image_size']
+    model_channels = config['model_channels']
+    max_channels = config['max_channels']
+    latent_dim = config['latent_dim']
+
+    learning_rate = config['learning_rate']
+    betas = config['beta1'], config['beta2']
+
+    generator = build_generator(image_channels, image_size, model_channels, max_channels, latent_dim)
+    discriminator = build_discriminator(image_channels, image_size, model_channels, max_channels)
 
     batch_size = config['batch_size']
     dataset = config['dataset']
     dataloader = numpy_dataloader(dataset, batch_size, 8)
 
-    learning_rate = config['learning_rate']
-    betas = config['beta1'], config['beta2']
-
-    latent_dim = config['latent_dim']
-    print_summary = config['print_summary']
-
     gan = GanModelBase(dataloader, generator, discriminator, latent_dim,
-                       lr=learning_rate, betas=betas, summary=print_summary)
+                       lr=learning_rate, betas=betas, summary=config['print_summary'])
     gan.batch_size = batch_size
     gan.gradient_updates = config['gradient_updates']
     gan.save_snapshot_rate = config['save_snapshot_rate']
