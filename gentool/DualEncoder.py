4be694c72670679e3f4afb865e1980ea0f4230a3
TheDudeFromCI <thedudefromci@gmail.com>
2021-02-12 08:11:38 -0700

    Created latent gan

---
+++ gentool/DualEncoder.py
@@ -0,0 +1,99 @@
+from math import log2
+
+from torch import nn
+from torch.optim.adam import Adam
+
+from Blocks import ResidualBlockDown, get_normalization_1d, get_activation
+from SubModuleBase import SubModuleBase
+
+
+class DualEncoder(SubModuleBase):
+    def __init__(self, config):
+        super().__init__(config['save_file'])
+
+        self.image_channels = config['image_channels']
+        self.image_size = config['image_size']
+
+        self.latent_dim_1 = config['latent_dim_1']
+        self.latent_dim_2 = config['latent_dim_2']
+
+        learning_rate = config['learning_rate'] if 'learning_rate' in config else 1
+        beta1 = config['beta1'] if 'beta1' in config else 0.9
+        beta2 = config['beta2'] if 'beta2' in config else 0.999
+
+        downsample_method = config['downsample_method']
+        model_channels = config['model_channels']
+        max_channels = config['max_channels']
+        skip_blocks = config['skip_blocks'] if 'skip_blocks' in config else 1
+        kernel = config['kernel'] if 'kernel' in config else 3
+
+        dense_layers_1 = config['dense_layers_1'] if 'dense_layers_1' in config else 1
+        dense_layers_2 = config['dense_layers_2'] if 'dense_layers_2' in config else 1
+
+        normalization = config['normalization']
+        activation = config['activation']
+
+        output_activation_1 = config['output_activation_1'] if 'output_activation_1' in config else activation
+        output_activation_2 = config['output_activation_2'] if 'output_activation_2' in config else activation
+
+        assert dense_layers_1 >= 1, "Cannot have negative dense layers!"
+        assert dense_layers_2 >= 1, "Cannot have negative dense layers!"
+        assert skip_blocks >= 1, "Cannot have negative skip layers!"
+        assert kernel >= 1, "Cannot have negative kernel size!"
+        assert kernel % 2 == 1, "Kernel must be an odd number value!"
+        assert self.image_size >= 4, "Image size must be at least 4!"
+        assert self.latent_dim_1 >= 1, "Latent dimensions must be at least 1!"
+        assert self.latent_dim_2 >= 1, "Latent dimensions must be at least 1!"
+
+        downsamples = int(log2(self.image_size)) - 2
+
+        def res_block():
+            blocks = []
+            for index in range(downsamples):
+                in_channels = min((1 << index) * 1 * model_channels, max_channels)
+                out_channels = min((1 << index) * 2 * model_channels, max_channels)
+
+                blocks.append(ResidualBlockDown(in_channels, out_channels,
+                                                self.image_size >> index, downsample_method, kernel))
+
+                for _ in range(skip_blocks - 1):
+                    blocks.append(ResidualBlockDown(out_channels, out_channels,
+                                                    self.image_size >> (index + 1), 'none', kernel))
+
+            return blocks
+
+        def dense_block(layers, latent, output_activation):
+            blocks = []
+
+            dense_channels = min((1 << downsamples) * model_channels, max_channels) * 4 * 4
+            blocks.append(nn.Linear(dense_channels, latent))
+
+            if layers > 1:
+                blocks.append(get_normalization_1d(normalization, latent))
+                blocks.append(get_activation(activation))
+
+            for i in range(layers - 1):
+                blocks.append(nn.Linear(latent, latent))
+
+                if i < layers - 2:
+                    blocks.append(get_normalization_1d(normalization, latent))
+                    blocks.append(get_activation(activation))
+
+            blocks.append(get_activation(output_activation))
+            return blocks
+
+        self.model = nn.Sequential(
+            nn.Conv2d(self.image_channels, model_channels, kernel, 1, kernel // 2),
+            *res_block(),
+
+            nn.Flatten(),
+        )
+
+        self.latent_1_model = nn.Sequential(*dense_block(dense_layers_1, self.latent_dim_1, output_activation_1))
+        self.latent_2_model = nn.Sequential(*dense_block(dense_layers_2, self.latent_dim_2, output_activation_2))
+
+        self.optimizer = Adam(self.parameters(), lr=learning_rate, betas=(beta1, beta2))
+
+    def forward(self, x):
+        x = self.model(x)
+        return self.latent_1_model(x), self.latent_2_model(x)
---
+++ gentool/LabeledGan.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+from torch import autograd
+from torch.cuda import FloatTensor
+from torch.nn import functional as F
+from torch.autograd.variable import Variable
+
+from GanModelBase import GanModelBase
+
+
+class LabeledGan(GanModelBase):
+    def __init__(self, dataloader, generator, discriminator, summary=False):
+        super().__init__(dataloader, generator, discriminator, summary=summary)
+
+    def wgan_gp_discriminator_loss(self, batch, labels, noise):
+        d_loss_real, d_latent_real = self.discriminator(batch)
+        d_loss_real = d_loss_real.mean()
+
+        generated = self.generator(noise).detach()
+        d_loss_fake, d_latent_fake = self.discriminator(generated)
+        d_loss_fake = d_loss_fake.mean()
+
+        gradient_penalty = self.calculate_gradient_penalty(batch.data, generated.data)
+
+        latent_loss_real = F.mse_loss(d_latent_real, labels)
+        latent_loss_fake = F.mse_loss(d_latent_fake, noise)
+        latent_loss = latent_loss_real + latent_loss_fake
+
+        return d_loss_fake - d_loss_real + gradient_penalty + latent_loss
+
+    def calculate_gradient_penalty(self, real_data, fake_data):
+        alpha = FloatTensor(np.random.random((real_data.size(0), 1, 1, 1)))
+        # Get random interpolation between real and fake samples
+        interpolates = (alpha * real_data + ((1 - alpha) * fake_data)).requires_grad_(True)
+        d_interpolates, _ = self.discriminator(interpolates)
+        fake = Variable(FloatTensor(real_data.shape[0], 1).fill_(1.0), requires_grad=False)
+        # Get gradient w.r.t. interpolates
+        gradients = autograd.grad(
+            outputs=d_interpolates,
+            inputs=interpolates,
+            grad_outputs=fake,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+        gradients = gradients.view(gradients.size(0), -1)
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        return gradient_penalty * self.gradient_penalty_lambda
+
+    def wgan_generator_loss(self, noise):
+        generated = self.generator(noise)
+        d_loss_fake, d_latent_fake = self.discriminator(generated)
+        d_loss_fake = d_loss_fake.mean()
+
+        latent_loss = F.mse_loss(d_latent_fake, noise)
+
+        return -d_loss_fake + latent_loss
+
+    def train_batch(self):
+        g_loss = 0
+        d_loss = 0
+
+        for _ in range(self.critic_updates):
+            self.discriminator.optimizer.zero_grad()
+
+            for _ in range(self.gradient_updates):
+                batch, labels = next(self.dataloader)
+                noise = self.random_latent()
+
+                loss = self.wgan_gp_discriminator_loss(batch, labels, noise)
+                loss.backward()
+
+                d_loss += loss.item() / self.gradient_updates / self.critic_updates
+
+            self.discriminator.optimizer.step()
+
+        self.generator.optimizer.zero_grad()
+        for _ in range(self.gradient_updates):
+            noise = self.random_latent()
+
+            loss = self.wgan_generator_loss(noise)
+            loss.backward()
+
+            g_loss += loss.item() / self.gradient_updates
+
+        self.generator.optimizer.step()
+
+        return '[g_loss: {:.6f}, d_loss: {:.6f}]'.format(g_loss, d_loss)
