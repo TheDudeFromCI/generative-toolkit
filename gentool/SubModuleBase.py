4dd1907066ea2dd5a3bdf1e27d4a86a8ebeccaf3
TheDudeFromCI <thedudefromci@gmail.com>
2021-02-12 08:09:37 -0700

    Created sub module.

---
+++ gentool/SubModuleBase.py
@@ -0,0 +1,19 @@
+import os
+import torch
+from torch import nn
+
+
+class SubModuleBase(nn.Module):
+    def __init__(self, save_file):
+        super().__init__()
+        self.save_file = save_file
+
+        if os.path.exists(self.save_file):
+            self.load_model()
+
+    def load_model(self):
+        state = torch.load(self.save_file)
+        self.load_state_dict(state)
+
+    def save_model(self):
+        torch.save(self.state_dict(), self.save_file)
