e4e7c4985a15a8cc5e16f8dd8fe82e87e303550d
TheDudeFromCI <thedudefromci@gmail.com>
2021-02-20 08:34:21 -0700

    Added gan ada.

--- gentool/GanModelBase.py
+++ gentool/GanModelBase.py
@@ -6,6 +6,7 @@ from torch.autograd.variable import Variable
 
 from ModelBase import ImageModelBase
 from GanImageSwapper import GanImageSwapper
+from AdaptiveDataAug import augment
 
 
 class GanModelBase(ImageModelBase):
@@ -17,7 +18,7 @@ class GanModelBase(ImageModelBase):
         self.dataloader = dataloader
 
         self.batch_size = 1
-        self.latent_dim = generator.latent_dim
+        self.latent_dim = generator.latent_dim if hasattr(generator, 'latent_dim') else 0
         self.image_size = generator.image_size
         self.image_channels = generator.image_channels
         self.sample_noise = self.noise((64, self.latent_dim))
@@ -26,15 +27,24 @@ class GanModelBase(ImageModelBase):
         self.swap_chance = 0.5
         self.swapper = None  # So we can have a chance to modify settings before start
 
+        self.ada_augment = torch.tensor([0.0, 0.0])
+        self.ada_target = 0.6
+        self.ada_length = 50000000
+        self.ada_aug_step = self.ada_target / self.ada_length
+        self.ada_p = 0
+
         self.critic_updates = 5
         self.gradient_penalty_lambda = 10
 
         if summary:
-            torchinfo.summary(self, (1, self.latent_dim))
+            self.summary()
 
-            params = self.count_params()
-            print(
-                f"Loaded GAN with {params['generator']:,} generator params and {params['discriminator']:,} discriminator params.")
+    def summary(self):
+        torchinfo.summary(self, (1, self.latent_dim))
+
+        params = self.count_params()
+        print(
+            f"Loaded GAN with {params['generator']:,} generator params and {params['discriminator']:,} discriminator params.")
 
     def forward(self, x):
         return self.discriminator(self.generator(x))
@@ -44,19 +54,33 @@ class GanModelBase(ImageModelBase):
         return images, 8
 
     def wgan_gp_discriminator_loss(self, batch, noise):
-        d_loss_real = self.discriminator(batch).mean()
+        batch, _ = augment(batch, self.ada_p)
+        d_loss_real = self.discriminator(batch)
 
         generated = self.generator(noise).detach()
         generated = self.swapper.swap(generated)
-
-        d_loss_fake = self.discriminator(generated).mean()
+        generated, _ = augment(generated, self.ada_p)
+        d_loss_fake = self.discriminator(generated)
 
         gradient_penalty = self.calculate_gradient_penalty(batch.data, generated.data)
+        self.update_ada_p(d_loss_real)
 
-        return d_loss_fake - d_loss_real + gradient_penalty
+        return d_loss_fake.mean() - d_loss_real.mean() + gradient_penalty
+
+    def update_ada_p(self, d_loss_real):
+        ada_aug_data = torch.tensor((torch.sign(d_loss_real).sum().item(), d_loss_real.shape[0]))
+        self.ada_augment += ada_aug_data
+        if self.ada_augment[1] > (self.batch_size * 4 - 1):
+            authen_out_signs, num_outputs = self.ada_augment.tolist()
+            r_t_stat = authen_out_signs / num_outputs
+            sign = 1 if r_t_stat > self.ada_target else -1
+            self.ada_p += sign * self.ada_aug_step * num_outputs
+            self.ada_p = min(1.0, max(0.0, self.ada_p))
+            self.ada_augment.mul_(0.0)
 
     def wgan_generator_loss(self, noise):
         generated = self.generator(noise)
+        generated, _ = augment(generated, self.ada_p)
         return -self.discriminator(generated).mean()
 
     def calculate_gradient_penalty(self, real_data, fake_data):
@@ -64,6 +88,7 @@ class GanModelBase(ImageModelBase):
         # Get random interpolation between real and fake samples
         interpolates = (alpha * real_data + ((1 - alpha) * fake_data)).requires_grad_(True)
         d_interpolates = self.discriminator(interpolates)
+
         fake = Variable(torch.ones((real_data.shape[0], 1)), requires_grad=False)
         # Get gradient w.r.t. interpolates
         gradients = autograd.grad(
@@ -74,7 +99,8 @@ class GanModelBase(ImageModelBase):
             retain_graph=True,
             only_inputs=True,
         )[0]
-        gradients = gradients.view(gradients.size(0), -1)
+        # gradients = gradients.view(gradients.size(0), -1)
+        gradients = gradients.reshape(gradients.size(0), -1)
         gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
         return gradient_penalty * self.gradient_penalty_lambda
 
